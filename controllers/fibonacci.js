acc1c212a9f5d17624ed12efeb7045aada6040c7
Jason D'souza <jvincentdsouza@gmail.com>
2019-03-18 21:22:26 +1100

    initial commit with the main functionality of the backend finished

---
+++ controllers/fibonacci.js
@@ -0,0 +1,32 @@
+const fibonacciCall = (req, resp) => {
+    const {numInput} = req.body;
+    // console.log(numInput);
+    
+    
+    if (!numInput || isNaN(Number(numInput)) || typeof numInput === 'undefined' || numInput === null) {
+        return resp.status(400).json('incorrect value submitted');
+    }
+
+    const fibonacciCalculate = (numInput) => {
+        let cache = {};
+        function fib(n){
+            if(n in cache) {
+                return cache[n]
+            } else {
+                if(n<2) {
+                    return n
+                }
+                cache[n] = fib(n-1) + fib(n-2)
+                return cache[n]
+            }
+        }
+        return fib(Number(numInput))
+    }
+    
+    resp.json(fibonacciCalculate(numInput))
+    
+}
+
+module.exports = {
+    fibonacciCall
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const fibonacci = require('./controllers/fibonacci');
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, resp) => {
+    resp.sendFile(path.join(__dirname + '/pages/index.html'))
+});
+
+app.post('/fibonacci', (req, resp) => {fibonacci.fibonacciCall(req, resp)})
+
+app.listen(port, () => {
+    console.log('app is running on ' + port);
+})
